3f44c97b47c517379445e913d4b353237229641f
Akash kumar <akaysharma894@gmail.com>
2021-02-05 00:03:21 +0530

    Added signup

---
+++ form-validation/src/components/FormSignup.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const FormSignup = () => {
+  return (
+    <div className="form-content-right">
+      <form className="form">
+        <h4>
+          Get started with us today! Create your account b filling out the
+          information below
+        </h4>
+      </form>
+    </div>
+  );
+};
+
+export default FormSignup;
