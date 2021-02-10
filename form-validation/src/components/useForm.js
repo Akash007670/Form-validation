e7bb2944a411c69b3f0661af57c99138c8b1a0fc
Akash kumar <akaysharma894@gmail.com>
2021-02-10 23:41:24 +0530

    Added state

---
+++ form-validation/src/components/useForm.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+
+const UseForm = () => {
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const [error, setError] = useState({});
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return { handleChange, values, handleSubmit };
+};
+
+export default UseForm;
